93a42aacfd6f7eecef1a1d9ac81de8894fee9e3f
FaganAhmadov <faganahmadov933@gmail.com>
2026-03-14 18:21:30 +0400

    animation_ panels

---
+++ Js-Tasks/DOM/panels/main.js
@@ -0,0 +1,7 @@
+const panels = document.querySelectorAll('.panel')
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        panels.forEach(item => item.classList.remove('active'))
+        panel.classList.add('active')
+    })
+})
